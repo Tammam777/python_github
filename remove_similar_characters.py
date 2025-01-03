466d2a4d141383f530d7ab7f7514daf6b3e3c6b7
Tammam <tammamalali696@gmail.com>
2025-01-03 17:29:19 +0400

    my first python project

---
+++ remove_similar_characters.py
@@ -0,0 +1,9 @@
+x="wwwwooorrrlllddd"
+def clenword(word):
+    if len(word) == 1:
+        return word
+    if word[0] == word[1]:
+        return clenword(word[1:])
+    return word[0] + clenword(word[1:])
+print(clenword(x))            
+
---
+++ student-grades.py
@@ -0,0 +1,20 @@
+if __name__ == '__main__':
+    records = []  # قائمة لتخزين بيانات الطلاب
+    for _ in range(int(input("أدخل عدد الطلاب: "))):
+        name = input("أدخل اسم الطالب: ")
+        score = float(input("أدخل درجة الطالب: "))
+        records.append([name, score])
+
+    # إيجاد ثاني أدنى درجة
+    scores = sorted(set([record[1] for record in records]))
+    second_lowest_score = scores[1]
+
+    # إيجاد الطلاب الحاصلين على ثاني أدنى درجة
+    students_with_second_lowest = [record[0] for record in records if record[1] == second_lowest_score]
+
+    # ترتيب الأسماء أبجديًا
+    students_with_second_lowest.sort()
+
+    # طباعة الأسماء
+    for student in students_with_second_lowest:
+        print(student)
